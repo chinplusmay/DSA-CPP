85b5f3dc9ee70774e2092655d60e639f12c81cf6
D Chinmay <zchinmayz@gmail.com>
2025-04-03 07:36:19 +0530

    MaximumOnes

---
+++ ProblemsArrays/Easy/MaximumConsecutiveOnes.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int findMaxConsecutiveOnes(vector<int>& nums) {
+    int n = nums.size();
+    int count = 0;
+    int max = 0;
+    for(int i = 0; i<n; i++){
+        if(nums[i] == 1){
+            count++;
+        }
+
+        if(count>max){
+            max = count;
+        }
+        if(nums[i] == 0){
+            count = 0;
+        }
+    }return max;
+}
+
+
+
+
+int main(){
+    vector<int> a = {1,1,0,1,1,1};
+    cout<<findMaxConsecutiveOnes(a);
+    return 0;
+}
