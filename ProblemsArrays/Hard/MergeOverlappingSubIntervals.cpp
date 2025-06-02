7720aaaaca74e27b90af3f86c7dc42891a027478
D Chinmay <zchinmayz@gmail.com>
2025-06-02 23:00:41 +0530

    mergeoverlapping optimal

--- ProblemsArrays/Hard/MergeOverlappingSubIntervals.cpp
+++ ProblemsArrays/Hard/MergeOverlappingSubIntervals.cpp
@@ -1,11 +1,12 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-vector<vector<int>> brute(vector<vector<int>> &arr) {
+vector<vector<int>> brute(vector<vector<int>> &arr) { // tc: O(NlogN)+O(2n)   sc:O(N)
     int n = arr.size();
+    vector<vector<int>> ans;
     //sort the array
     sort(arr.begin(), arr.end());
-    vector<vector<int>> ans;
+    
     for(int i = 0; i<n; i++){
         int start = arr[i][0];
         int end = arr[i][1];
@@ -24,12 +25,29 @@ vector<vector<int>> brute(vector<vector<int>> &arr) {
         ans.push_back({start, end});
     }
     return ans;
-
 }
+    
+    vector<vector<int>> optimal(vector<vector<int>> &arr) { //tc:O(N*logN) + O(N)=> O(N)    sc:O(N)
+    int n = arr.size();
+    vector<vector<int>> ans;
+    //sort the array
+    sort(arr.begin(), arr.end());
+    
+    for(int i = 0; i<n; i++){
+        if(ans.empty() || arr[i][0] > ans.back()[1]){
+            ans.push_back({arr[i][0], arr[i][1]});
+        }
+        else{ // if it is overlapping
+            ans.back()[1] = max(arr[i][1], ans.back()[1]);
+        }
+    }
+    return ans;
+}
+
 
 int main(){
     vector<vector<int>> arr = {{1, 3}, {8, 10}, {2, 6}, {15, 18}};
-    vector<vector<int>> ans = brute(arr);
+    vector<vector<int>> ans = optimal(arr);
     cout << "The merged intervals are: " << "\n";
     for (auto it : ans) {
         cout << "[" << it[0] << ", " << it[1] << "] ";
